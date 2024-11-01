c7078619d2b47f9534b662fbe6afd4b5916d94a2
santiago.gomez1 <santiago.gomez1@alumnos.uneatlantico.es>
2024-11-01 11:26:00 +0100

    Adding projects

--- profesional-homepage/src/Components/Proyecto.js
+++ profesional-homepage/src/Components/Proyecto.js
@@ -1,19 +1,31 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import { works } from '../data/works'
+import React, { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import { works } from '../data/works';
 
 export const Proyecto = () => {
-
-    const [proyecto, setProyecto] = useState({})
-    const params = useParams()
+    const [proyecto, setProyecto] = useState({});
+    const { id } = useParams();
+    const navigate = useNavigate();
 
     useEffect(() => {
-        let proyecto = works.filter(work => work.id === params.id)
-        setProyecto(proyecto[0])
-    }, [])
-  return (
-    <div className='page'>
-        <h1 className='heading'>{params.nombre}</h1>
-    </div>
-  )
-}
+        const selectedProject = works.find(work => work.id === id);
+        setProyecto(selectedProject || {});
+    }, [id]);
+
+    if (!proyecto) return <p>Proyecto no encontrado</p>;
+
+    return (
+        <div className='page'>
+          <h1 className='project-heading'>{proyecto.nombre}</h1>
+          <span className='project-category'>{proyecto.categorias}</span>
+          <h3 className='project-tech'>{proyecto.tecnologias}</h3>
+          <p className='project-description'>{proyecto.descripcion}</p>
+          <a href={proyecto.url} target='_blank' rel='noopener noreferrer' className='project-link'>
+              Ver Proyecto
+          </a>
+          <div>
+            <button onClick={() => navigate(-1)}>Regresar al Portafolio</button>
+          </div>
+        </div>  
+    );
+};
--- profesional-homepage/src/Router/MyRoutes.js
+++ profesional-homepage/src/Router/MyRoutes.js
@@ -26,6 +26,7 @@ export const MyRoutes = () => {
             <Route path="*" element={
               <div className='page'>
                 <h1 className='heading'>Error 404</h1>
+                <p className='paragraph'>La página que estás buscando no existe</p>
               </div>
             }></Route>
           </Routes>
