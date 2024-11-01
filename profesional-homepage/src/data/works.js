ceddfe698bd732a2e816ec07f7aee81feda35776
santiago.gomez1 <santiago.gomez1@alumnos.uneatlantico.es>
2024-11-01 10:40:54 +0100

    Adding projects

--- profesional-homepage/src/data/works.js
+++ profesional-homepage/src/data/works.js
@@ -1,6 +1,6 @@
 export const works = [
     {
-        'id': '0',
+        'id': 'react-start',
         'nombre': 'React Start',
         'url': 'https://github.com/Santi1503/ReactStart',
         'tecnologias': 'React',
@@ -8,7 +8,7 @@ export const works = [
         'descripcion': 'Este proyecto es un conjunto de proyectos desarrollados en React en donde se ve mi aprendizaje en pequeños módulos. Esto es para la gente que este aprendiendo React desde cero para que se pueda hacer una idea de los componentes que se utilizan en React.'
     },
     {
-        'id': '1',
+        'id': 'start-up',
         'nombre': 'Start Up',
         'url': 'https://start-up-three.vercel.app/',
         'tecnologias': 'React',
@@ -16,7 +16,7 @@ export const works = [
         'descripcion': 'El proyecto esta enfocado en hacer un resumen de las habilidades que tengo actualmente y tener un registro de los proyectos que he estado elaborando. Como tal, es algo parecido a un portafolio, ya que es un intento de primer portafolio.'
     },
     {
-        'id': '2',
+        'id': 'python-projects',
         'nombre': 'Proyectos de Python',
         'url': 'https://github.com/Santi1503/ProyectosPython',
         'tecnologias': 'Python',
@@ -24,7 +24,7 @@ export const works = [
         'descripcion': 'Son varios proyectos desarrollados en Python en los cuales se desglosa en varias áreas. Estas incluyen Machine Learning, Bots de ayuda y pequeñas Aplicaciones.'
     },
     {
-        'id': '3',
+        'id': 'swift-projects',
         'nombre': 'Proyectos de Swift',
         'url': 'https://github.com/Santi1503/Swift-projects',
         'tecnologias': 'Swift',
@@ -32,11 +32,35 @@ export const works = [
         'descripcion': 'Son varios proyectos desarrollados en Swift y SwiftUI en los que se puede ver mi experiencia con tecnologías para desarrollo móvil, en este caso para dispositivos IOS.'
     },
     {
-        'id': '4',
+        'id': 'csharp-projects',
         'nombre': 'Proyectos de CSharp',
         'url': 'https://github.com/Santi1503/CSharp-Projects',
         'tecnologias': 'CSharp',
         'categorias': 'Desarrollo CSharp',
         'descripcion': 'Estos son varios proyectos que he desarrollado en CSharp con los cursos de Microsoft. Aquí de igual manera se encontrarán proyectos de métodos que se utilizan en CSharp.'
+    },
+    {
+        'id': 'machineLearning-dataScience',
+        'nombre': 'Machine Learning & Data Science',
+        'url': 'https://github.com/Santi1503/machineLearning-dataScience',
+        'tecnologias': 'Python',
+        'categorias': 'Desarrollo Python',
+        'descripcion': 'Aquí se puede encontrar varios proyectos que he realizado tras aprender un curso de Machine Learning & Data Science.'
+    },
+    {
+        'id': 'docker',
+        'nombre': 'Proyectos de Docker',
+        'url': 'https://github.com/Santi1503/DockerProyects',
+        'tecnologias': 'Docker',
+        'categorias': 'Desarrollo Docker',
+        'descripcion': 'Aquí se puede encontrar varios proyectos que he realizado para reforzar mis conocimientos de Docker. Son algunos proyectos que he realizado antes pero ahora utilizando Docker.'
+    },
+    {
+        'id': 'threejs',
+        'nombre': 'Threejs Journey',
+        'url': 'https://github.com/Santi1503/Threejs-journey',
+        'tecnologias': 'Threejs',
+        'categorias': 'Desarrollo Threejs',
+        'descripcion': 'Se detalla  el proceso de aprender Threejs y los proyectos que he realizado en el camino.'
     }
 ]
