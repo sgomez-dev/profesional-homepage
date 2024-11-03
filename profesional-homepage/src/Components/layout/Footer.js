3409c9532e49722ef2438cf48a359ded4ab16ca2
santiago.gomez1 <santiago.gomez1@alumnos.uneatlantico.es>
2024-11-03 23:33:11 +0100

    Adding social media

--- profesional-homepage/src/Components/layout/Footer.js
+++ profesional-homepage/src/Components/layout/Footer.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
 
 export const Footer = () => {
   return (
     <footer className="footer">
-      Santiago Gómez de la Torre &copy; Developer - 2024
+      <div className="social-icons">
+        Santiago Gómez de la Torre &copy; Developer - 2024
+        <a
+          href="https://www.instagram.com/santigt1503/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={require("../../Images/instagram.png")} alt="Intagram" />
+        </a>
+        <a
+          href="www.linkedin.com/in/santiago-gomez-de-la-torre-3a7b62249"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={require("../../Images/linkedin.png")} alt="Linkedin" />
+        </a>
+        <a
+          href="https://github.com/Santi1503"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={require("../../Images/github.png")} alt="Github" />
+        </a>
+      </div>
     </footer>
-  )
-}
+  );
+};
